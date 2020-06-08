183b98097abb960490a9af9bf2bb74af07a178ab
jonatan <jonatankruszewski@itc.tech>
2020-06-08 15:12:27 +0300

    Small Fix on Test Name

--- tests/D01/D01-EX01.test.js
+++ tests/D01/D01-EX01.test.js
@@ -1,6 +1,6 @@
 import { printHelloWorld } from "../../D01/EX01";
 describe("Testing D01EX02", () => {
-  test("Prints Hello World!", () => {
+  test("Prints Hello World", () => {
     global.console = {
       log: jest.fn(),
     };
